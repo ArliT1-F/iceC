8509e62da769865a9591afa7c186961cbee1b3b6
Karli911 <35976946+Karli911@users.noreply.github.com>
2023-02-11 13:44:26 +0100

    main.py update and added .env
    soooo i wanted to make the token hidden but it keeps passing an error everytime i add the token in the .env file. since im so stupid ill take some time off to check how it works and then fix it

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
 import discord
 from discord.ext import commands
 
+import os
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+TOKEN = os.getenv("DISCORD_TOKEN")
+
 intents = discord.Intents.default()
 intents.message_content = True
-client = discord.Client(intents = intents)
+
+client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
-  print(f'{client.user} is online!')
-  
-client.run('token')
+  print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run('NzY5NTUzNDcwNjAwMTE4Mjgz.GVK2B6._SIutMAnPTtlUB4iiDoJOgl01AmwRshYFhhyy4')
